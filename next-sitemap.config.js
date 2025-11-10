f5a11cabda00f11f6e3c28670ab5869dcf25d211
Tasneem Abdelrazek <tasneem.ar.work@gmail.com>
2025-11-10 21:33:30 +0200

    fixed /

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://my-portfolio-nu-snowy-64.vercel.app/',
+  siteUrl: 'https://my-portfolio-nu-snowy-64.vercel.app',
   generateRobotsTxt: true,
 };
